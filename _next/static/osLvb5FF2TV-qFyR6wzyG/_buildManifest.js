3b2bd8a3f622026832287c5dd8531df20d3d919b
gianfaye <git@gianfaye.com>
2023-03-02 11:06:37 +0000

    Deploying to gh-pages from @ gianfaye/undeciphered-clothing-web@bfeace092d1055658733ee8e62c40a48d0671f18 🚀

---
+++ _next/static/osLvb5FF2TV-qFyR6wzyG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/54856ec7197cb0d6.css","static/chunks/pages/index-c51388fd4942ba9a.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
